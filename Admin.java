ad9945e453c63a1713f97c10467525d6fd26475c
AtakannD <72988818+AtakannD@users.noreply.github.com>
2023-12-24 16:43:49 +0300

    Creation of register, login process, and implementing database.

--- Admin.java
+++ Admin.java
@@ -1,3 +1,6 @@
 public class Admin extends User{
-	
+
+    public Admin(String name, String surname, String email, String password) {
+        super(name, surname, email, password);
+    }
 }
--- AuctionSystem.java
+++ AuctionSystem.java
@@ -1,31 +1,43 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Scanner;
 import java.io.IOException;
 
 public class AuctionSystem {
+
+
     private Scanner scanner;
+    private User user;
 
     private AuctionSystem(){
         this.scanner = new Scanner(System.in);
     }
 
+    String url = "jdbc:mysql://localhost:3306/auction_system";
+    String username = "root";
+    String password = "";
+
     public static void main(String[] args) {
         AuctionSystem auctionSystem = new AuctionSystem();
         auctionSystem.run();
-    }
 
+    }
     private void run(){
         System.out.println("Welcome to the Auction System!");
+
         while (true) {
             displayMenu();
             int option = getValidOption();
             switch (option) {
-                case 1:
-                    login();
-                    break;
-                case 0:
+                case 1 -> login();
+                case 2 -> register();
+                case 0 -> {
                     System.out.println("Exiting the Auction System. Goodbye!");
                     scanner.close();
                     System.exit(0);
+                }
             } // Switch Ending
         } // While Ending
     } // Method Ending
@@ -33,6 +45,7 @@ public class AuctionSystem {
     private void displayMenu() {
         System.out.println("\nChoose an option:");
         System.out.println("1. Login");
+        System.out.println("2. Register");
         System.out.println("0. Quit");
     }
 
@@ -56,10 +69,79 @@ public class AuctionSystem {
         return option;
     }
 
-    private void login(){
-        System.out.println("Login Page.");
+    private void login() {
+
+        System.out.println("Enter e-mail address: ");
+        String emailAddress = scanner.nextLine();
+
+        System.out.println("Enter your password: ");
+        String userPassword = scanner.nextLine();
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Statement statement = connection.createStatement();
+
+            // Check if there is a user with the provided email and password
+            String query = "SELECT * FROM user WHERE user_email = '" + emailAddress + "' AND user_password = '" + userPassword + "'";
+            ResultSet resultSet = statement.executeQuery(query);
+
+            if (resultSet.next()) {
+                // User is authenticated
+                System.out.println("Login successful!");
+                // You can add additional logic or call other methods here
+            } else {
+                // Authentication failed
+                System.out.println("Invalid email or password. Please try again.");
+            }
+
+            connection.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
+    private void register() {
+        System.out.println("Enter your first name: ");
+        String firstName = scanner.nextLine();
+
+        System.out.println("Enter your last name: ");
+        String lastName = scanner.nextLine();
+
+        System.out.println("Enter your e-mail address: ");
+        String emailAddress = scanner.nextLine();
+
+        System.out.println("Enter your password: ");
+        String userPassword = scanner.nextLine();
+
+        System.out.println("Enter your user type (e.g., buyer as B, seller as S): ");
+        String userType = scanner.nextLine();
+
+        //System.out.println("Enter your user id: ");
+       // String userId = scanner.nextLine();
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Statement statement = connection.createStatement();
+
+            // Insert the new user into the database
+            String insertQuery = "INSERT INTO user (user_name, user_surname, user_email, user_password, user_type) " +
+                    "VALUES ('" + firstName + "', '" + lastName + "', '" + emailAddress + "', '" + userPassword + "', '" + userType + "')";
+            int rowsAffected = statement.executeUpdate(insertQuery);
+
+            if (rowsAffected > 0) {
+                System.out.println("Registration successful!");
+                // You can add additional logic or call other methods here
+            } else {
+                System.out.println("Registration failed. Please try again.");
+            }
+
+            connection.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 
 
 }
