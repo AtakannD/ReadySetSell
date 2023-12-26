6ebdf2c80e9dfc61f3e87bc29f8c1f3e3cd8b12a
AtakannD <72988818+AtakannD@users.noreply.github.com>
2023-12-26 19:05:41 +0300

    Developed Buyer & Seller methods

--- Buyer.java
+++ Buyer.java
@@ -12,6 +12,7 @@ public class Buyer extends User{
     String password = "";
     List<String> searchList;
     List<String> auctionList;
+    Auction auctions;
 
     // Auction currentAuction;
 
@@ -62,8 +63,10 @@ public class Buyer extends User{
                     String itemName = searchResultSet.getString("item_name");
                     int itemQuantity = searchResultSet.getInt("item_quantity");
                     int itemPrice = searchResultSet.getInt("base_price");
+                    int auctionId = searchResultSet.getInt("auction_id");
 
-                    String resultString = itemQuantity + "x " + itemName + " for " + itemPrice + "TL";
+                    String resultString = itemQuantity + "x " + itemName + " for " + itemPrice + "TL and items " +
+                            "belongs to auction " + auctionId;
                     searchList.add(resultString);
                 }
 
@@ -93,10 +96,9 @@ public class Buyer extends User{
     }
 
     private void joinAuction() {
-        /*
         System.out.println("Please enter Auction Id: ");
 		int auctionId = scanner.nextInt();
-
+        /*
         boolean found = false;
         for (Auction auction : auctions) {
             if (auction.getAuctionId() == auctionId) {
@@ -110,29 +112,37 @@ public class Buyer extends User{
             System.out.println("\nNo auction found.");
         }
          */
+
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection(url, username, password);
             Statement statement = connection.createStatement();
 
-            String searchQueryForItems = "SELECT *  FROM items";
+            String searchQueryForItems = "SELECT *  FROM items WHERE auction_id = '" + auctionId + "'";
             ResultSet auctionListResult = statement.executeQuery(searchQueryForItems);
 
             auctionList = new ArrayList<>();
             while (auctionListResult.next()) {
+                String resultString;
                 String itemName = auctionListResult.getString("item_name");
                 int itemQuantity = auctionListResult.getInt("item_quantity");
                 int itemPrice = auctionListResult.getInt("base_price");
                 int itemId = auctionListResult.getInt("item_id");
-                String resultString = itemId + ". " + itemQuantity + "x " + itemName + " " + itemPrice + "TL";
+                int bids = auctionListResult.getInt("bids");
+                if (bids > itemPrice) {
+                    resultString = itemId + ". " + itemQuantity + "x " + itemName + " " + itemPrice + "TL but highest bid is " + bids + "TL";
+                } else {
+                    resultString = itemId + ". " + itemQuantity + "x " + itemName + " " + itemPrice + "TL";
+                }
                 auctionList.add(resultString);
             }
 
             if (!auctionList.isEmpty()) {
-                System.out.println("You can bid on: ");
+                System.out.println("These items are in the auction: ");
                 for (String itemName : auctionList) {
                     System.out.println(itemName);
                 }
+                bidOnItem();
             } else {
                 System.out.println("No items found in auction right now.");
             }
@@ -142,7 +152,59 @@ public class Buyer extends User{
         }
     }
 
-    private void watchAuction() {
+    private void bidOnItem() {
+        System.out.println("Please enter the name of the item you want to bid on:");
+        String itemName = scanner.nextLine();
 
+        // Validate that the item name is not empty
+        while (itemName.trim().isEmpty()) {
+            System.out.println("Item name cannot be empty. Please enter a valid item name:");
+            itemName = scanner.nextLine();
+        }
+
+        System.out.println("Please enter your bid. Remember your bid needs to be higher than the base price or previous bids.");
+        int newBidOnItem = scanner.nextInt();
+        scanner.nextLine();  // Consume the newline character left by nextInt()
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Statement statement = connection.createStatement();
+
+            String currentBidQuery = "SELECT bids, base_price FROM items WHERE item_name ='"+ itemName +"'";
+            ResultSet resultSet = statement.executeQuery(currentBidQuery);
+            if (resultSet.next()) {
+                int currentBids = resultSet.getInt("bids");
+                int currentBasePrice = resultSet.getInt("base_price");
+                // Check if the new bid is higher
+                if (newBidOnItem > currentBids && newBidOnItem > currentBasePrice) {
+                    String bidQueryForItems = "UPDATE items SET bids = GREATEST(bids, ?) WHERE item_name = ?";
+                    try (PreparedStatement preparedStatement = connection.prepareStatement(bidQueryForItems)) {
+                        preparedStatement.setInt(1, newBidOnItem);
+                        preparedStatement.setString(2, itemName);
+
+                        // Execute the update
+                        int rowsAffectedItems = preparedStatement.executeUpdate();
+
+                        if (rowsAffectedItems > 0) {
+                            System.out.println("Bidding is successful. You can follow the auction with the Watch Auction option.");
+                            getActionsByUser();
+                        } else {
+                            System.out.println("Bidding failed. Please try again.");
+                        }
+                    }
+                } else {
+                    System.out.println("Your bid must be higher than the current bids and base price.");
+                    bidOnItem();
+                }
+            } else {
+                System.out.println("Item not found. You are directing buyer menu.");
+                getActionsByUser();
+            }
+
+            connection.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
--- Seller.java
+++ Seller.java
@@ -1,7 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.Scanner;
+
 public class Seller extends User{
+
+    Scanner scanner;
 
     public Seller(String userEmail, String userPassword) {
         super(userEmail, userPassword);
+        scanner = new Scanner(System.in);
     }
 
     public void displaySellerMenu() {
@@ -10,5 +18,70 @@ public class Seller extends User{
         System.out.println("2. Remove Item");
         System.out.println("3. Watch Auction");
         System.out.println("0. Quit");
+    }
+
+    public void getActionsBySeller() {
+        displaySellerMenu();
+        int option = getValidOption();
+        switch (option) {
+            case 1 -> addItem();
+            case 2 -> removeItem();
+            case 3 -> watchAuction();
+        }
+    }
+
+    private void addItem() {
+        System.out.println("Please enter your items name: ");
+        String itemName = scanner.nextLine();
+        System.out.println("Please enter your items' quantity: ");
+        String itemsQuantity = scanner.nextLine();
+        System.out.println("Please decide your items' base price: ");
+        Integer itemsBasePrice = scanner.nextInt();
+        System.out.println("Please enter your items' auction number which belongs to: ");
+        Integer itemsAuctionNumber = scanner.nextInt();
+        Integer lowerBid = itemsBasePrice;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Statement statement = connection.createStatement();
+
+            String itemAddingQuery = "INSERT INTO items (item_name, item_quantity, base_price, auction_id, bids)" +
+                    "VALUES ('" + itemName + "', '" + itemsQuantity + "', '" + itemsBasePrice + "', '" + itemsAuctionNumber + "', '" + lowerBid + "')";
+            int rowsAffertedItems = statement.executeUpdate(itemAddingQuery);
+
+            if (rowsAffertedItems > 0) {
+                System.out.println("Item addition is succesfull!");
+                System.out.println("You are directing menu again.");
+                getActionsBySeller();
+            } else {
+                System.out.println("Items addition failed. Please try again.");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    private void removeItem() {
+        System.out.println("Please enter items' name which you want to remove: ");
+        String itemsName = scanner.nextLine();
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Statement statement = connection.createStatement();
+
+            String itemRemovingQuery = "DELETE FROM items WHERE item_name = '" + itemsName + "'";
+            int rowsAffectedItems = statement.executeUpdate(itemRemovingQuery);
+
+            if (rowsAffectedItems > 0) {
+                System.out.println("Item removing is succesfull!");
+                System.out.println("You are directing menu again.");
+                getActionsBySeller();
+            } else {
+                System.out.println("Items removing failed. Please try again.");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
